a84b1c5f53321126224b74dc28887405df94b879
michi <michi@MICHIEL-PC>
2021-07-09 09:41:54 +0200

    Add project files.

---
+++ Hello_Xamarin/Hello_Xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Hello_Xamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Hello_Xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
